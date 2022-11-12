d18f394e5e866483ae2b1a1097234d0bdc91c5cd
jawadmughal079 <88715926+jawadmughal079@users.noreply.github.com>
2022-11-12 03:06:14 -0800

    Add files via upload

---
+++ deleteAnyNode.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node()
+    {
+        data = 0;
+        next = NULL;
+    }
+    Node(int data)
+    {
+        this->data = data;
+        next = NULL;
+    }
+};
+class LinkList
+{
+    Node *head;
+
+public:
+    LinkList()
+    {
+        head = NULL;
+    }
+    void insertAtBegin(int data)
+    {
+        if (head == NULL)
+        {
+            /* code */
+            cout << endl
+                 << "LinkList is empty so we insert values at 0 index " << endl;
+            Node *newNode = new Node(data);
+            head = newNode;
+            newNode->next = NULL;
+        }
+        else
+        {
+
+            // question is this why we not use the temp there becuase we want to chnage the postion of head that why
+            Node *newNode = new Node(data);
+            newNode->next = head;
+            head = newNode;
+
+            cout << endl
+                 << "Value Insert at the begning " << endl;
+        }
+    }
+
+    void print()
+    {
+        if (head == NULL)
+        {
+            cout << "WE CAN NOT PRINT THE VALUES BECAUSE LINK LIST IS EMPTY ";
+        }
+        else
+        {
+            Node *temp = head;
+            while (temp != NULL)
+            {
+                /* code */
+                cout << endl;
+                cout << temp->data;
+                // cout << endl;
+                temp = temp->next;
+            }
+        }
+    }
+
+    void deleteFromAnyNode(int postion)
+    {
+
+        Node *temp = head;
+        int ct = 1;
+        while (ct < postion - 1)
+        {
+            temp = temp->next;
+            ct++;
+        }
+
+        Node *temp2 = temp->next;
+        temp->next = temp->next->next;
+        delete temp2;
+    }
+};
+int main()
+{
+    LinkList l;
+    l.insertAtBegin(1);
+    l.insertAtBegin(2);
+    l.insertAtBegin(3);
+    l.insertAtBegin(4);
+    l.insertAtBegin(5);
+    l.insertAtBegin(6);
+    l.print();
+    l.deleteFromAnyNode(3);
+    // l.deleteFromAnyNode(3);
+    // l.deleteFromAnyNode(3);
+    // l.deleteFromAnyNode(3);
+    cout << "PRINT THE VALUES \n ";
+    l.print();
+}
